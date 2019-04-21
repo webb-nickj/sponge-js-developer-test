354129a7c046db69c28ae95593b32d2a3fcbbf1f
Nick Webb <nick.webb@internationalworkplace.com>
2019-04-21 14:04:20 +0100

    Task 10: task runner
    Setup up grunt to comile LESS.
    Updated package.json and created Gruntfile.js
    Spent some time looking at options for configuring the LESS task and testing.
    45 minutes

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    
+  grunt.initConfig({
+
+    "less" : {
+
+      "development": {
+
+        "options": {
+          "sourceMap" : true,
+          "sourceMapFileInline" : true
+        },
+
+        "files": {
+          'app/css/spongetest-less.css': 'app/less/spongetest-less.less'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('default', ['less'] );
+
+}
